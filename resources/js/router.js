ff19e8c9c793a0f95d97bbf652085b1d93f694ad
MirAzim <sayedazemullahalhashemi1020@gmail.com>
2026-03-31 18:28:47 +0430

    this is some changes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,19 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+// Pages
 import Home from './pages/Home.vue'
 import About from './pages/About.vue'
-
-// pages
+import Contact from './pages/Contact.vue'
+import Accounts from './pages/accounts/index.vue'
 
 const routes = [
+
+  {
+    path: "/accounts",
+    name: "Accounts",
+    component: Accounts ,
+    meta: {
+      title: "حساب‌ها",
+      subtitle: "مدیریت حساب‌ها",
+    },
+  },
+
+
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'خانه',
+      subtitle: 'به صفحه اصلی خوش آمدید',
+    },
   },
   {
     path: '/about',
     name: 'About',
     component: About,
+    meta: {
+      title: 'درباره ما',
+      subtitle: 'معلومات در مورد ما',
+    },
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
+    meta: {
+      title: 'تماس با ما',
+      subtitle: 'با ما در ارتباط باشید',
+    },
   },
 ]
 
