94ce910b2a1f51301d051ec1776342de942d6ba3
MasihArifi <masiharifi943@gmail.com>
2026-04-02 16:31:31 +0430

    this is the chnages for the expense category

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\Api\ExpenseCategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('accounts', AccountController::class);
+Route::apiResource('expense-categories', ExpenseCategoryController::class);
