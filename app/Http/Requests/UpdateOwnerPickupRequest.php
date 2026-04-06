e14d62e6bbfa4a321a3a10d2ce7e9b48d34f52d0
MasihArifi <masiharifi943@gmail.com>
2026-04-06 16:58:40 +0430

    this is the changes for th expense price maker

--- app/Http/Requests/UpdateOwnerPickupRequest.php
+++ app/Http/Requests/UpdateOwnerPickupRequest.php
@@ -10,10 +10,7 @@ class UpdateOwnerPickupRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+
 
     /**
      * Get the validation rules that apply to the request.
