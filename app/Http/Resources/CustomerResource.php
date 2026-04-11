6c6e31b685c56262d6c8ff9c96927968f5e9ace8
barikziy <barikziy.ahmadzai2022@gmail.com>
2026-04-11 17:36:27 +0430

    this is the customer changes

---
+++ app/Http/Resources/CustomerResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'last_name' => $this->last_name,
+            'father_name' => $this->father_name,
+            'phone_number' => $this->phone_number,
+            'tazkira_number' => $this->tazkira_number,
+            'image' => $this->image,
+            'note' => $this->note,
+        ];
+    }
+}
